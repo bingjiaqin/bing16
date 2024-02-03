7cdf418ce211e3bbbc8b6153886ece423a2cfe6f
qbj <bingjiaqin@foxmail.com>
2024-02-03 21:44:25 +0800

    -a

--- src/components/index/PicConfig.js
+++ src/components/index/PicConfig.js
@@ -1,13 +1,7 @@
-import i1 from "@/assets/picture/index/image/1.jpg";
-import i2 from "@/assets/picture/index/image/2.jpg";
-import i3 from "@/assets/picture/index/image/3.jpg";
-import i4 from "@/assets/picture/index/image/4.jpg";
-import i5 from "@/assets/picture/index/image/5.jpg";
-
 export const PIC_LIST = [
-    { "src": i1, "title": "【2019年春】人们在看舞龙"},
-    { "src": i2, "title": "【2016年夏】没有人知道人生的列车开往何处"},
-    { "src": i3, "title": "【2019年夏】工业与禁锢"},
-    { "src": i4, "title": "【2018年夏】山雾·天欲雨"},
-    { "src": i5, "title": "【2022年春】关于夜晚窗外的灯照映在房顶上的图案"}
+    { "src": "https://bing16.dynv6.net:1443/WOGied.jpg", "title": "【2019年春】人们在看舞龙"},
+    { "src": "https://bing16.dynv6.net:1443/XJH9BJ.jpg", "title": "【2016年夏】没有人知道人生的列车开往何处"},
+    { "src": "https://bing16.dynv6.net:1443/EDfb7O.jpg", "title": "【2019年夏】工业与禁锢"},
+    { "src": "https://bing16.dynv6.net:1443/ogHQUf.jpg", "title": "【2018年夏】山雾·天欲雨"},
+    { "src": "https://bing16.dynv6.net:1443/90usRv.jpg", "title": "【2022年春】关于夜晚窗外的灯照映在房顶上的图案"}
 ];
