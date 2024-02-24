f9ac7fcd2fd8d34147fe8bf45731ebefedb15537
qbj <bingjiaqin@foxmail.com>
2024-02-24 15:40:16 +0800

    添加build mode

--- vite.config.js
+++ vite.config.js
@@ -6,27 +6,35 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]}
-    ),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: "/bing16/",
-  build: {
-    outDir: "docs"
-  },
-  server: {
-    host: '0.0.0.0'
-  },
-  assetsInclude: ["**/*.JPG"],
+export default defineConfig(({ command, mode }) => {
+  let base;
+  if (mode === 'gh') {
+    base = '/bing16/'
+  } else if (mode === 'cf') {
+    base = '/'
+  }
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]}
+      ),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: base,
+    build: {
+      outDir: "docs"
+    },
+    server: {
+      host: '0.0.0.0'
+    },
+    assetsInclude: ["**/*.JPG"],
+  }
 })
