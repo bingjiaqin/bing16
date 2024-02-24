95d9d8c5039cd9b58253b108b59766c518d0b742
qbj <bingjiaqin@foxmail.com>
2024-02-24 17:13:08 +0800

    添加路由配置

---
+++ src/components/markdown/Article.vue
@@ -0,0 +1,9 @@
+<script setup>
+const text = "# 1234";
+</script>
+
+<template>
+    <v-md-editor v-model="text" height="400px" mode="preview"></v-md-editor>
+</template>
+
+<style scoped>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/element/index.scss'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import vhCheck from "vh-check"
+import { createApp } from 'vue';
+import App from './App.vue';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
+import vhCheck from "vh-check";
 vhCheck();
 
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+import Prism from 'prismjs';
+
+VueMarkdownEditor.use(vuepressTheme, {
+    Prism,
+});
+
 const app = createApp(App)
+
+import router from './router/index'
+app.use(router)
+import './styles/element/index.scss';
+
 app.mount('#app')
+app.use(VueMarkdownEditor);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
