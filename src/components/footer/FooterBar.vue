a1bea2ebd7b602a3cedac1f7cef499dd4519ae0f
qbj <bingjiaqin@foxmail.com>
2024-04-05 08:43:18 +0800

    修改域名

--- src/components/footer/FooterBar.vue
+++ src/components/footer/FooterBar.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="footer">
-    © 2018-2024 <a href="https://bing16.pages.dev">Bing16.</a> All rights reserved.
+    © 2018-2024 <a href="https://bing16.xyz">Bing16.</a> All rights reserved.
     <a target="_blank" title="51la网站统计" href="https://v6.51.la/land/KAB7DS6mDtzdJLC4">
       <img src="https://sdk.51.la/icon/2-2.png" style="height: 13px" alt="51la网站统计">
     </a>
