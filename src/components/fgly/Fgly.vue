e64265b94cf35bdf551ecafd09d1ec0710f89edc
Bingjia Qin <bingjia@remnants.cc>
2026-05-04 16:11:41 +0800

    feat: wyzx & fgly 卡片改瀑布流布局 (CSS columns masonry)

--- src/components/fgly/Fgly.vue
+++ src/components/fgly/Fgly.vue
@@ -7,47 +7,31 @@ const filteredData = ref(data.itmes);
 </script>
 
 <template>
-  <div class="content">
-    <el-row :gutter="20">
-      <el-col
-          v-for="(item, index) in filteredData"
-          :key="index"
-          :xs="24"
-          :sm="12"
-          :md="8"
-          :lg="6"
-          style="text-align: center; margin-bottom: 20px"
-      >
-        <div class="card" :body-style="{ padding: '0px' }">
-          <div style="padding: 14px">
-            <h3 style="margin-bottom: 20px;">{{ item.title }}</h3>
-            <div class="bottom" style="margin-bottom: 20px;">
-              <time class="time">{{ item.intro }}</time>
-            </div>
-            <div style="margin-bottom: 20px;">[{{ item.group }}]</div>
-            <div v-if="item.type==='md'">
-              <router-link :to="{ path: `/article/${item.href}` }" target="_blank">查看</router-link>
-            </div>
-            <div v-else>
-              <a :href="item.href" target="_blank">查看</a>
-            </div>
+  <div class="masonry">
+    <div
+        v-for="(item, index) in filteredData"
+        :key="index"
+        class="masonry-item"
+    >
+      <div class="card">
+        <div style="padding: 14px">
+          <h3 style="margin-bottom: 20px;">{{ item.title }}</h3>
+          <div class="bottom" style="margin-bottom: 20px;">
+            <time class="time">{{ item.intro }}</time>
+          </div>
+          <div style="margin-bottom: 20px;">[{{ item.group }}]</div>
+          <div v-if="item.type==='md'">
+            <router-link :to="{ path: `/article/${item.href}` }" target="_blank">查看</router-link>
+          </div>
+          <div v-else>
+            <a :href="item.href" target="_blank">查看</a>
           </div>
         </div>
-      </el-col>
-    </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-@import '../../styles/_card.scss';
-
-.content {
-  padding: 100px 60px;
-}
-.el-col {
-  padding-top: 30px;
-}
-.card {
-  border-radius: 8px;
-}
+@import '../../styles/_masonry.scss';
 </style>
--- src/components/wyzx/Wyzx.vue
+++ src/components/wyzx/Wyzx.vue
@@ -7,52 +7,36 @@ const filteredData = ref(data);
 </script>
 
 <template>
-  <div class="content">
-    <el-row :gutter="20">
-      <el-col
-          v-for="(item, index) in filteredData"
-          :key="index"
-          :xs="24"
-          :sm="12"
-          :md="8"
-          :lg="6"
-          style="text-align: center; margin-bottom: 20px"
-      >
-        <div class="card" :body-style="{ padding: '0px' }">
-          <el-image :src="item.pic">
-            <template #error>
-              <el-image :src="item.pic.replace('lsky.remnants.cc','lskyv4.remnants.cc')">
-              </el-image>
-            </template>
-          </el-image>
-          <div style="padding: 14px">
-            <h3>{{ item.title }}</h3>
-            <div class="bottom">
-              <time class="time">{{ item.intro }}</time>
-            </div>
-            <div v-if="item.type==='md'">
-              <router-link :to="{ path: `/article/${item.href}` }" target="_blank">查看</router-link>
-            </div>
-            <div v-else>
-              <a :href="item.href" target="_blank">查看</a>
-            </div>
+  <div class="masonry">
+    <div
+        v-for="(item, index) in filteredData"
+        :key="index"
+        class="masonry-item"
+    >
+      <div class="card">
+        <el-image :src="item.pic">
+          <template #error>
+            <el-image :src="item.pic.replace('lsky.remnants.cc','lskyv4.remnants.cc')">
+            </el-image>
+          </template>
+        </el-image>
+        <div style="padding: 14px">
+          <h3>{{ item.title }}</h3>
+          <div class="bottom">
+            <time class="time">{{ item.intro }}</time>
+          </div>
+          <div v-if="item.type==='md'">
+            <router-link :to="{ path: `/article/${item.href}` }" target="_blank">查看</router-link>
+          </div>
+          <div v-else>
+            <a :href="item.href" target="_blank">查看</a>
           </div>
         </div>
-      </el-col>
-    </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-@import '../../styles/_card.scss';
-
-.content {
-  padding: 100px 60px;
-}
-.el-col {
-  padding-top: 30px;
-}
-.card {
-  border-radius: 8px;
-}
+@import '../../styles/_masonry.scss';
 </style>
